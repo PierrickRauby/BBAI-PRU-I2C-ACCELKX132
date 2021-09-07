c0c11fbe200f6f2063ccd9b5e3b39defaa2f4816
PierrickRauby <pierrick.rauby@gadz.org>
2021-09-07 18:28:31 +0000

    reading memory need to save in file :

--- user_space/read_mem/read_mem.c
+++ user_space/read_mem/read_mem.c
@@ -1,58 +1,43 @@
-
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#define PRU_SHAREDMEM        0x4B210000      // Start of PRU memory Page 184 am335x TRM
 
-#define PRU_ADDR        0x4B200000      // Start of PRU memory Page 184 am335x TRM
-#define PRU_LEN         0x80000         // Length of PRU memory
-#define PRU0_DRAM       0x00000         // Offset to DRAM
-#define PRU1_DRAM       0x02000
-#define PRU_SHAREDMEM   0x10000         // Offset to shared memory
 
-unsigned int    *pru0DRAM_32int_ptr;        // Points to the start of local DRAM
-unsigned int    *pru1DRAM_32int_ptr;        // Points to the start of local DRAM
-unsigned int    *prusharedMem_32int_ptr;    // Points to the start of the shared memory
 
-int main(int argc, char *argv[]) {
-  unsigned int    *pru;       // Points to start of PRU memory.
-  int fd;
-  printf("Reading shared memory");
-  fd = open ("/dev/mem", O_SYNC); // only need to read the memory
-  if (fd == -1) {
-    printf ("ERROR: could not open /dev/mem.\n\n");
-    return 1;
+
+int main() {
+
+  off_t offset = 0x4B210000;
+  size_t len = 2048;
+
+  // Truncate offset to a multiple of the page size, or mmap will fail.
+  size_t pagesize = sysconf(_SC_PAGE_SIZE);
+  off_t page_base = (PRU_SHAREDMEM / pagesize) * pagesize;
+  off_t page_offset = PRU_SHAREDMEM - page_base;
+
+  int fd = open("/dev/mem", O_SYNC);
+  unsigned char *mem = mmap(NULL, page_offset + len, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, page_base);
+  if (mem == MAP_FAILED) {
+    perror("Can't map memory");
+    return -1;
   }
-  pru = mmap(0, PRU_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, PRU_ADDR);
-  if (pru == MAP_FAILED) {
-    printf ("ERROR: could not map memory.\n\n");
-    return 1;
+
+  size_t i;
+  int16_t combined;
+  float result;
+  uint8_t msb,lsb;
+  for (i = 0; i < len; i+=2){
+    lsb=mem[page_offset + i];
+    msb=mem[page_offset + i+1];
+    combined = (msb << 8 ) | (lsb & 0xff);
+    result=0.0002441407513657033*(float)combined;
+    printf("  result: %0.6f\n",result);
   }
-  close(fd);
-  printf ("Using /dev/mem.\n");
-  prusharedMem_32int_ptr = pru + PRU_SHAREDMEM/4; // Points to start of shared memory
-
-  printf("%02x ", prusharedMem_32int_ptr[0]);
-  /*
-    off_t offset = strtoul(argv[1], NULL, 0);
-    size_t len = strtoul(argv[2], NULL, 0);
-
-    // Truncate offset to a multiple of the page size, or mmap will fail.
-    size_t pagesize = sysconf(_SC_PAGE_SIZE);
-    off_t page_base = (offset / pagesize) * pagesize;
-    off_t page_offset = offset - page_base;
-
-    int fd = open("/dev/mem", O_SYNC);
-    unsigned char *mem = mmap(NULL, page_offset + len, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, page_base);
-    if (mem == MAP_FAILED) {
-        perror("Can't map memory");
-        return -1;
-    }
-
-    size_t i;
-    for (i = 0; i < len; ++i)
-        printf("%02x ", (int)mem[page_offset + i]);
-*/
-    return 0;
+  /*printf("%02x ", mem[page_offset + i]);}*/
+  printf("%02x ",combined);
+  return 0;
 }
--- user_space/user_space.c
+++ user_space/user_space.c
@@ -35,25 +35,61 @@
 
 
 #include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/poll.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
+#define PRU_SHAREDMEM        0x4B210000      // Start of PRU memory Page 184 am335x TRM
 #define MAX_BUFFER_SIZE        512
 char readBuf[MAX_BUFFER_SIZE];
-
 #define DEVICE_NAME        "/dev/rpmsg_pru30"
+
+
+int readmemory() {
+  off_t offset = 0x4B210000;
+  size_t len = 2048;
+  // Truncate offset to a multiple of the page size, or mmap will fail.
+  size_t pagesize = sysconf(_SC_PAGE_SIZE);
+  off_t page_base = (PRU_SHAREDMEM / pagesize) * pagesize;
+  off_t page_offset = PRU_SHAREDMEM - page_base;
+  int output_file = open("./acquisition.csv", O_RDWR);
+  if (output_file == NULL) {
+    printf("Error, cannot open ");
+    exit(1);
+  }
+  int fd = open("/dev/mem", O_SYNC);
+  unsigned char *mem = mmap(NULL, page_offset + len, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, page_base);
+  if (mem == MAP_FAILED) {
+    perror("Can't map memory");
+    return -1;
+  }
+  size_t i;
+  int16_t combined;
+  float result;
+  uint8_t msb,lsb;
+  for (i = 0; i < len-2; i+=2){
+    lsb=mem[page_offset + i];
+    msb=mem[page_offset + i+1];
+    combined = (msb << 8 ) | (lsb & 0xff);
+    result=0.0002441407513657033*(float)combined;
+    printf("  result: %0.6f\n",result);
+  }
+  return 0;
+}
+
 
 int main(void)
 {
+    /* Trigger the Acquisition by sending information to the PRU*/
     struct pollfd pollfds[1];
     /*int i;*/
     long result = 0;
-
     /* Open the rpmsg_pru character device file */
     pollfds[0].fd = open(DEVICE_NAME, O_RDWR);
-
     /*
      * If the RPMsg channel doesn't exist yet the character device
      * won't either.
@@ -64,22 +100,27 @@ int main(void)
         printf("Failed to open %s\n", DEVICE_NAME);
         return -1;
     }
-
     /* The RPMsg channel exists and the character device is opened */
-    printf("Opened %s, sending address to read to PRU\n", DEVICE_NAME);
+    printf("Opened %s, starting acquisition \n", DEVICE_NAME);
     /* In this example only the adress to read with the PRU is sent*/
     result = write(pollfds[0].fd, "Place Add", 10);
     if (result > 0)
-      printf("Waiting for PRU answer through character device %s\n", DEVICE_NAME);
-
+      printf("Waiting for acquisition to complete on: %s\n", DEVICE_NAME);
     /* Poll until we receive a message from the PRU and then print it */
     result = read(pollfds[0].fd, readBuf, 11);
-    if (result > 0)
-      printf("Answer received from PRU:\n\n %s\n\n", readBuf);
-
-
+    char str[1];
+    sprintf(str, "%s", readBuf);
+    if (result > 0){
+      if(!strcmp(str,"0")){
+        printf("Acquisition sucess, retrieving results");
+      }else{
+        printf("Acquisition error\n");
+      }
+    }
     /* Close the rpmsg_pru character device file */
     close(pollfds[0].fd);
 
+    /* Read results from memory and save them in a csv*/
+    readmemory();
     return 0;
 }
